a92b8fced86cb33883f55abb50397c9b0db14f2b
karaiyaashu <34779290+karaiyaashu@users.noreply.github.com>
2020-11-20 04:42:58 +0530

    File writing complete

--- webscrapping.py
+++ webscrapping.py
@@ -1,20 +1,41 @@
 from bs4 import BeautifulSoup
 import requests
+import time
+
+print("Put some skills you are not familiar with")
+
+unfamiliar_skill = input(">")
+
+print(f"filtering out {unfamiliar_skill} ")
 
 
-html_text = requests.get(
-    'https://www.timesjobs.com/candidate/job-search.html?searchType=Home_Search&from=submit&asKey=OFF&txtKeywords=&cboPresFuncArea=35').text
+def find_jobs():
+    html_text = requests.get(
+        "https://www.timesjobs.com/candidate/job-search.html?searchType=Home_Search&from=submit&asKey=OFF&txtKeywords=&cboPresFuncArea=35"
+    ).text
 
-soup = BeautifulSoup(html_text, 'lxml')
+    soup = BeautifulSoup(html_text, "lxml")
+    jobs = soup.find_all("li", class_="clearfix job-bx wht-shd-bx")
 
-jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
+    for index, job in enumerate(jobs):
+        published_date = job.find("span", class_="sim-posted").span.text
+        if "today" in published_date:
+            company_name = job.find("h3", class_="joblist-comp-name").text.replace(
+                " ", ""
+            )
+            skills = job.find("span", class_="srp-skills").text.replace(" ", "")
+            more_info = job.header.h2.a["href"]
+            if unfamiliar_skill not in skills:
+                with open(f"posts/{index}.txt", "w") as f:
+                    f.write(f"company name: {company_name.strip()}")
+                    f.write(f"Required skills: {skills.strip()}\n")
+                    f.write(f"More info : {more_info}\n")
+                print(f"file saved{index}")
 
-for job in jobs:
-    published_date = job.find('span', class_='sim-posted').span.text
-    if 'today' in published_date:
-        company_name = job.find(
-            'h3', class_='joblist-comp-name').text.replace(' ', '')
-        skills = job.find('span', class_='srp-skills').text.replace(' ', '')
-        print(f'company name: {company_name.strip()}')
-        print(f'Required skills: {skills.strip()}\n')
-        # print(company_name)
+
+if __name__ == "__main__":
+    while True:
+        find_jobs()
+        time_wait = 10
+        print(f"Waiting {time_wait} minutes ")
+        time.sleep(time_wait * 60)
